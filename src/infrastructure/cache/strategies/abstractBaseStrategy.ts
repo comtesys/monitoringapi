befa7863de2f33bf4787a70b59cd24fccd222a1e
Martin Sura <suramartin@gmail.com>
2022-12-19 14:18:06 +0100

    oprava nove verze typescriptu

--- src/infrastructure/cache/strategies/abstractBaseStrategy.ts
+++ src/infrastructure/cache/strategies/abstractBaseStrategy.ts
@@ -3,7 +3,7 @@ import { IStorage } from "../storages/storage";
 export abstract class AbstractBaseStrategy {
   constructor(protected storage: IStorage) {}
 
-  public abstract async getItem<T>(key: string): Promise<T>;
-  public abstract async setItem(key: string, content: any, options: any): Promise<void>;
-  public abstract async clear(): Promise<void>;
+  public abstract getItem<T>(key: string): Promise<T>;
+  public abstract setItem(key: string, content: any, options: any): Promise<void>;
+  public abstract clear(): Promise<void>;
 }
